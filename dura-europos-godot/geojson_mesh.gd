c54a549ae706b262e9556d6d5dda77d5df835ee3
earth418 <alihafez42@gmail.com>
2025-02-03 14:37:01 -0500

    spawn spheres on geojson

--- dura-europos-godot/geojson_mesh.gd
+++ dura-europos-godot/geojson_mesh.gd
@@ -24,14 +24,17 @@ func load_json_file(json_filepath):
 		generate_geojson_mesh()
 		refresh = false
 
-var center_loc = [34.74799827813365, 40.73026895370481]
+#const center_loc = [34.74799827813365, 40.73026895370481]
+const center_loc = [34.748, 40.730]
+
+var temp_objs : Array[MeshInstance3D] = []
 
 func lat_lon_to_cartesian(loc):
 	# Returns the lat/lon as a vector3 relative to the origin
 	
-	#print(loc)
+	#print(center_loc)
 	
-	var radius = 6371000 # meters
+	var radius = 6371 * 0.2 # meters
 	
 	var loc1 = [rad_to_deg(center_loc[0]), rad_to_deg(center_loc[1])]
 	var loc2 = [rad_to_deg(loc[0]), rad_to_deg(loc[1])]
@@ -41,12 +44,19 @@ func lat_lon_to_cartesian(loc):
 	var x = 	cos(loc1[0]) * sin(loc2[0]) - sin(loc1[0]) * cos(loc2[0]) \
 		* cos(loc2[1] - loc1[1])
 		
-	return radius * Vector3(x, y, 0.0)
+	return radius * Vector3(x, 0.0, y)
 	
 func generate_geojson_mesh():
+	
+	for temp_obj in temp_objs:
+		remove_child(temp_obj)
+		#temp_obj.free()
+	temp_objs.clear()
+	
 	#load_json_file()
 	var geojson = json_contents.data
-	#mesh = ArrayMesh.new()
+
+	var scene_root = get_tree().root.get_children()[0]
 	
 	for feature in geojson["features"]:
 		var feature_name = feature["properties"]["id"]
@@ -55,11 +65,22 @@ func generate_geojson_mesh():
 		
 		var coordinates = feature["geometry"]["coordinates"]
 		
+		#var i = 0
 		for coordinate in coordinates[0]:
-			print("hi ", coordinate)
-			#print(lat_lon_to_cartesian([coordinate[0], coordinate[1]]))
-	
-	pass
+			#i += 1
+			#if i > 10:
+				#return
+			var coordvec = [coordinate[1], coordinate[0]]
+			#print("hi ", coordvec)
+			var loc = lat_lon_to_cartesian(coordvec) + Vector3(0.0, 20.0, 0.0)
+			print(loc)
+			var new_sphere = $sphere.duplicate()
+			#var new_sphere = MeshInstance3D.new()
+			#new_sphere.mesh = new_sphere_mesh
+			add_child(new_sphere)
+			new_sphere.global_position = loc
+			#temp_objs.append(new_sphere)
+			#print("\n")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
