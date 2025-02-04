362429b65132299328d0159748a12da015aa653a
Ali Hafez <alihafez42@gmail.com>
2025-02-04 04:40:54 -0500

    land and everything to scale perfectly :)

--- dura-europos-godot/geojson_mesh.gd
+++ dura-europos-godot/geojson_mesh.gd
@@ -24,39 +24,50 @@ func load_json_file(json_filepath):
 		generate_geojson_mesh()
 		refresh = false
 
-#const center_loc = [34.74799827813365, 40.73026895370481]
-const center_loc = [34.748, 40.730]
+@export var clear_dots : bool:
+	set(new_val):
+		for temp_obj in $test.get_children(true):
+			$test.remove_child(temp_obj)
+		clear_dots = false
 
-var temp_objs : Array[MeshInstance3D] = []
+
+const center_loc = [34.74799827813365, 40.73026895370481]
+#const center_loc = [34.748, 40.730]
+
+#var temp_objs : Array[MeshInstance3D] = []
+
+
+func lat_lon_to_cartesian2(loc):
+	var radius = 6371 * 0.2 # meters
+	
+	#var loc1 = [rad_to_deg(center_loc[0]), rad_to_deg(center_loc[1])]
+	#var loc2 = [rad_to_deg(loc[0]), rad_to_deg(loc[1])]
+	#
+	#var x1 = asin(cos())
 
 func lat_lon_to_cartesian(loc):
 	# Returns the lat/lon as a vector3 relative to the origin
-	
+	#return lat_lon_to_cartesian2(loc)
 	#print(center_loc)
 	
-	var radius = 6371 * 0.2 # meters
+	var radius = 6371000 # meters, * 0.5 because our 
 	
-	var loc1 = [rad_to_deg(center_loc[0]), rad_to_deg(center_loc[1])]
-	var loc2 = [rad_to_deg(loc[0]), rad_to_deg(loc[1])]
+	var loc1 = [deg_to_rad(center_loc[0]), deg_to_rad(center_loc[1])]
+	var loc2 = [deg_to_rad(loc[0]), deg_to_rad(loc[1])]
 
 	
 	var y = sin(loc2[1] - loc1[1]) * cos(loc2[0]) 
 	var x = 	cos(loc1[0]) * sin(loc2[0]) - sin(loc1[0]) * cos(loc2[0]) \
 		* cos(loc2[1] - loc1[1])
 		
-	return radius * Vector3(x, 0.0, y)
+	return radius * Vector3(y, 0.0, -x)
 	
 func generate_geojson_mesh():
 	
-	for temp_obj in temp_objs:
-		remove_child(temp_obj)
-		#temp_obj.free()
-	temp_objs.clear()
-	
-	#load_json_file()
+	for temp_obj in $test.get_children(true):
+		$test.remove_child(temp_obj)
+		
 	var geojson = json_contents.data
-
-	var scene_root = get_tree().root.get_children()[0]
 	
 	for feature in geojson["features"]:
 		var feature_name = feature["properties"]["id"]
@@ -72,12 +83,12 @@ func generate_geojson_mesh():
 				#return
 			var coordvec = [coordinate[1], coordinate[0]]
 			#print("hi ", coordvec)
-			var loc = lat_lon_to_cartesian(coordvec) + Vector3(0.0, 20.0, 0.0)
+			var loc = lat_lon_to_cartesian(coordvec) + Vector3(0.0, 250.0, 0.0)
 			print(loc)
 			var new_sphere = $sphere.duplicate()
 			#var new_sphere = MeshInstance3D.new()
 			#new_sphere.mesh = new_sphere_mesh
-			add_child(new_sphere)
+			$test.add_child(new_sphere)
 			new_sphere.global_position = loc
 			#temp_objs.append(new_sphere)
 			#print("\n")
