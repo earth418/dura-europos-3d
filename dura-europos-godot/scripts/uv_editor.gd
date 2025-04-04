0d5cee4615cafbeffa0ad1b3202349128e2484ee
Ali Hafez <alihafez42@gmail.com>
2025-04-04 04:52:09 -0400

    IT WORKSSS LETS GO

--- dura-europos-godot/scripts/uv_editor.gd
+++ dura-europos-godot/scripts/uv_editor.gd
@@ -27,9 +27,12 @@ static func create_editor(object, picture, pic_info):
 var uv_pos_list : Array = []
 
 @onready var uv_itemlist : ItemList = camera.get_node("Control/ItemList")
+@onready var polygon : Polygon2D = $Control2/TextureRect/Polygon2D
+@onready var space: PhysicsDirectSpaceState3D = new_world.get_world_3d().direct_space_state
 
 @onready var geojson_mesh = preload("res://scenes/geoJSON_mesh.tscn")
-@onready var texture_mat  = preload("res://materials/tex_display.tres")
+@onready var texture_mat  = preload("res://materials/white_standard_mat.tres")
+@onready var highlight_mat = preload("res://materials/white_highlight_mat.tres")
 
 @export var object : Node
 
@@ -40,9 +43,6 @@ func _ready() -> void:
 	$Control2/SubViewportContainer/SubViewport.size = $Control2.size * Vector2(0.5, 1.0)
 	grab_focus()
 	var pos = Vector3(0, 0.0, 0)
-	#$Control2/SubViewportContainer/SubViewport/Node3D.position = pos
-	#$Control2/SubViewportContainer/SubViewport/FreeLookOrbitCamera.anchor_transform = Transform3D(Basis(), pos)
-	
 	camera.anchor_transform = Transform3D(Basis(), pos)
 	#world_3d = World3D.new()
 	
@@ -64,22 +64,17 @@ func _ready() -> void:
 		
 		print(new_obj)
 		print(new_obj.json_path)
-		new_obj.global_position = pos
-		
+		new_obj.global_position = pos	
 	else:
 		object = new_world.get_node("Area3D")
 		## debug
-		#new_obj = MeshInstance3D.new()
-		#var box = BoxMesh.new()
-		#box.size = Vector3(50, 50, 50)
-		#new_obj.mesh = box
-		
 	#new_world.add_child(new_obj)
 	if _picture:
 		(texture_rect.texture as ImageTexture).set_image(_picture)
 	else:
 		texture_rect.texture = load("res://assets/satellite_color_square.png")
-	#pass # Replace with function body.
+	
+	uv_itemlist.item_selected.connect(list_item_clicked)
 
 
 enum PickState {TEX, VIEWPORT, DONE}
@@ -88,6 +83,8 @@ var state : PickState = PickState.DONE
 var working_uvs : Array[Vector2]
 var working_pos : Array[Vector3]
 var working_intersect : Dictionary
+
+var selected_indices : Dictionary
 
 var _mouse_position : Vector2
 
@@ -111,32 +108,41 @@ func _input(event: InputEvent) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if state == PickState.TEX:
-		var panel : Panel = $Control2/TextureRect/Panel
-		if len(uv_pos_list) > 0:
-			var last_uv = uv_pos_list[-1][0]
-			panel.position = last_uv
-			panel.size = _mouse_position - last_uv
+		#var panel : Panel = $Control2/TextureRect/Panel
 		
-	#var sub_vp = $Control2/SubViewportContainer/SubViewport
-	#var vp_texture : ViewportTexture = sub_vp.get_texture()
-	#texture_rect.texture = vp_texture
-	#var vp_image = vp_texture.get_image()
-	#(texture_rect.texture as ImageTexture).set_image(vp_image)
+		
+		if len(working_uvs) > 0:
+			var pts = PackedVector2Array()
+			for i in working_uvs:
+				pts.append(i * texture_rect.size)
+			var mp = get_mouse_position() - texture_rect.position
+			pts.append(mp)
+			print(pts)
+			polygon.polygon = pts
+		else:
+			polygon.polygon.clear()
+	else:
+		if state == PickState.VIEWPORT:
+			var pts = PackedVector2Array()
+			for i in working_uvs:
+				pts.append(i * texture_rect.size)
+			polygon.polygon = pts
+		
 
 func _on_close_requested() -> void:
 	hide()
 	
 func _on_texture_input(event: InputEvent) -> void:
-	if event.is_action("left-click") and state == PickState.TEX:
+	if event.is_action_pressed("left-click") and state == PickState.TEX:
 		
 		var i = get_mouse_position()
 		var base = texture_rect.position
 		working_uvs.append((i - base) / texture_rect.size)
 		#print(uv)
+		print(len(working_uvs), " uvs clicked out of 4!!")
 		if len(working_uvs) == 4:
 			state = PickState.VIEWPORT
 
-@onready var space: PhysicsDirectSpaceState3D = new_world.get_world_3d().direct_space_state
 
 func get_mouse_point():
 	var sub_vp_container = $Control2/SubViewportContainer
@@ -157,22 +163,12 @@ func get_mouse_point():
 	ray_query.to = src_pos + 1000.0 * cam_dir
 	#var intersect = space.intersect_ray(ray_query)
 	return space.intersect_ray(ray_query)
-	#if intersect:
-		#var point = intersect["position"]
-		#var normal = intersect["normal"]
-		#
-		##print(point)
-		##new_world.get_node("MeshInstance3D2").global_position = point
-		#return point
-	#else:
-		#return -Vector3.INF
 
 func _on_viewport_container_input(event: InputEvent) -> void:
 	#return
 	#var root = $Control2/SubViewportContainer/SubViewport/Node3D
 	#var camera = root.get_node("FreeLookCamera")
-	if event.is_action("left-click"):
-		if state == PickState.VIEWPORT:
+	if event.is_action_pressed("left-click") and state == PickState.VIEWPORT:
 			var intersect = get_mouse_point()
 			if intersect:
 				if not working_intersect:
@@ -180,6 +176,7 @@ func _on_viewport_container_input(event: InputEvent) -> void:
 				
 				if intersect["normal"].dot(working_intersect["normal"]) > 0.95:
 					working_pos.append(intersect["position"])
+					print(len(working_pos), " points clicked out of 4!!")
 					if len(working_pos) == 4:
 						apply_texture()
 				else:
@@ -188,77 +185,75 @@ func _on_viewport_container_input(event: InputEvent) -> void:
 				
 func apply_texture():
 	
-	var clicked_object = working_intersect["collider"]
+	var clicked_object : Node3D = working_intersect["collider"]
 	#var clicked_point : Vector3 = working_intersect["position"]
 	var clicked_normal : Vector3 = working_intersect["normal"]
 	for i in range(len(working_pos)):
 		print("associating uv ", working_uvs[i], " with 3d point ", working_pos[i])
 	#uv_list[uv] = clicked_point
-	uv_pos_list.append([working_uvs, working_pos])
-	uv_itemlist.add_item("texture 1")
+		
+	var material : StandardMaterial3D = texture_mat #.duplicate()
+	material.albedo_texture = texture_rect.texture
 	
-	for i in range(0, len(uv_pos_list) - 1, 2):
+	
+	var new_mesh : ArrayMesh
+	var new_meshinstance : MeshInstance3D
+	
+	if clicked_object.has_node("addeds"):
+		new_meshinstance = clicked_object.get_node("addeds") as MeshInstance3D
+		new_mesh = new_meshinstance.mesh
+	else:
+		new_meshinstance = MeshInstance3D.new()
+		new_meshinstance.name = "addeds"
 		
-		var c1 = uv_pos_list[i] # c1[0] is the UV, c1[1] is the vector
-		var c2 = uv_pos_list[i + 1] # c2[0] is the UV, c2[1] is the vector
+		new_mesh = ArrayMesh.new()
+		new_meshinstance.mesh = new_mesh
 		
-		#var old_tex = texture_rect.texture.get_image()
-		##old_tex.crop()
-		#var old_tex_size = Vector2(old_tex.get_size())
-		
-		# making the material
-		#var min_uv = Vector2i(c1[0].min(c2[0]) * old_tex_size)
-		#var max_uv = Vector2i(c1[0].max(c2[0]) * old_tex_size)
-		##var offset = min_uv 
-		#var scale = (max_uv - min_uv)
-		
-		var material : StandardMaterial3D = texture_mat.duplicate()
-		#var region = Rect2i(offset, scale) 
-		
-		#var new_tex_data = []
-		##
-		#var old_tex_data = old_tex.get_data()
-		#var start_index : int = int(offset.y) * old_tex_size.x + int(offset.x)
-		#var end_index : int = start_index + int(scale.y) * old_tex_size.x + int(scale.x)
-		
-		#for row in range(min_uv.x, max_uv.x):
-			#var row_start_index = row * scale.y
-			#new_tex_data.append_array(old_tex_data.slice(row_start_index, row_start_index + scale.y))
-		#print(len(old_tex.data["data"]))
-		#print(old_tex.data)
-		
-		#var new_tex_data = old_tex_data.slice(start_index, end_index)
-		#print("Starting at i=",start_index," until i=", end_index)
-		#print("new data: ", new_tex_data)
-		#var tex_img = old_tex.get_region(region)
-		#print(len(new_tex_data))
-		#print("new image: ( ", scale.x, " x ", scale.y ," )")
-		
-		#var tex_img = Image.create_from_data(scale.x, scale.y, true, old_tex.get_format(), new_tex_data)
-		#print(region)
-		#var new_tex = ImageTexture.create_from_image(tex_img)
-		#new_tex.set_image(tex_img)
-		material.albedo_texture = texture_rect.texture
-		
-		var center = (c1[1] + c2[1]) / 2.0
-		var extent = c2[1] - c1[1]
-		var side_length = (extent.length() * sqrt(2.0)) / 2.0
-		
-		var new_plane = PlaneMesh.new()
-		new_plane.size = Vector2.ONE * side_length
-		var new_plane_mesh = MeshInstance3D.new()
-		clicked_object.add_child(new_plane_mesh)
-		new_plane_mesh.mesh = new_plane
-		#new_plane_mesh.basis.from_euler()
-		#new_plane_mesh.global_position = center
-		#new_plane_mesh.quaternion = Quaternion(clicked_normal, 0.0)
-		new_plane_mesh.look_at_from_position(center, center + Vector3.FORWARD, clicked_normal)
-		new_plane_mesh.rotate(clicked_normal.cross(Vector3.UP), PI / 2.0)
-		new_plane_mesh.position += clicked_normal * 1.01
-		new_plane_mesh.set_surface_override_material(0, material)
+		clicked_object.add_child(new_meshinstance)
+	
+	var arrays = []
+	arrays.resize(ArrayMesh.ARRAY_MAX)
+	arrays[ArrayMesh.ARRAY_TEX_UV] = PackedVector2Array(working_uvs)
+	arrays[ArrayMesh.ARRAY_VERTEX] = PackedVector3Array(working_pos)
+	arrays[ArrayMesh.ARRAY_NORMAL] = PackedVector3Array([clicked_normal, clicked_normal, clicked_normal, clicked_normal])
+	
+	# 0, 1, 2, 0, 2, 3
+	var indices = [2, 1, 0, 3, 2, 0] if clicked_normal.dot(Vector3.UP) > 0.95 else [0, 1, 2, 0, 2, 3]
+	arrays[ArrayMesh.ARRAY_INDEX] = PackedInt32Array(indices)
+	
+	var surface_idx = new_mesh.get_surface_count()
+	new_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	new_mesh.surface_set_material(surface_idx, material)
+	
+	
+	
+	uv_pos_list.append([working_uvs, working_pos, new_mesh, surface_idx])
+	uv_itemlist.add_item("Decal " + str(uv_itemlist.item_count + 1))
+	
+	#new_plane_mesh.basis.from_euler()
+	#new_plane_mesh.global_position = center
+	#new_plane_mesh.quaternion = Quaternion(clicked_normal, 0.0)
+	#new_meshinstance.look_at_from_position(center, center + Vector3.FORWARD, clicked_normal)
+	#new_meshinstance.rotate(clicked_normal.cross(Vector3.UP), PI / 2.0)
+	new_meshinstance.position += clicked_normal * 0.05
+	#new_meshinstance.set_surface_override_material(0, material)
 		#new_plane_mesh.po
 	
 	state = PickState.DONE
 	working_intersect = {}
 	working_pos = []
 	working_uvs = []
+
+func list_item_clicked(index : int):
+	var new_mat : Material
+	if index in selected_indices:
+		new_mat = texture_mat
+		selected_indices.erase(index)
+	else:
+		new_mat = highlight_mat
+		selected_indices[index] = 1
+		
+	var mesh : ArrayMesh = uv_pos_list[index][2]
+	var surface_idx = uv_pos_list[index][3]
+	new_mat.albedo_texture = texture_rect.texture
+	mesh.surface_set_material(surface_idx, new_mat)
