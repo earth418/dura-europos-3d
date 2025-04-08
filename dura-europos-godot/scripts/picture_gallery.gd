da47a74c7acc3ad9bd9f3b00cd072b7a838393a5
Ali Hafez <alihafez42@gmail.com>
2025-04-08 13:22:45 -0400

    fixed up some query stuff

--- dura-europos-godot/scripts/picture_gallery.gd
+++ dura-europos-godot/scripts/picture_gallery.gd
@@ -22,17 +22,41 @@ func add_customs():
 	for i in custom_pictures:
 		pass
 
+func set_httpsignal(new_function):
+	
+	for c in httpr.request_completed.get_connections():
+		httpr.request_completed.disconnect(c)
+	httpr.request_completed.connect(new_function)
+
 func add_custom_image(image_id):
+	#Q122677095
+	var image_query = "https://query.wikidata.org/sparql?query=SELECT%20%3Fitem%20%3Flabel%20%3FlabelAlt%20%3Fdescription"+\
+	"%20%3Fimage%20%7B%0A%20%20BIND(wd%3A"+\
+	image_id +\
+	"%20AS%20%3Fitem)%0A%20%20%3Fitem%20wdt%3AP18%20%3Fimage.%0A%20%20SERVICE%20wikibase"+\
+	"%3Alabel%20%7B%0A%20%20%20%20bd%3AserviceParam%20wikibase%3Alanguage%20%22en%2Car%22.%0A%20%20%20%20%3Fitem%20rdfs"+\
+	"%3Alabel%20%3Flabel.%20%0A%20%20%20%20%3Fitem%20skos%3AaltLabel%20%3FlabelAlt.%0A%20%20%20%20%3Fitem%20schema%3Adescription%20%3Fdescription%0A%20%20%7D%0A%7D%0A"
 	
-	var image_query = "https://query.wikidata.org/sparql?query=SELECT%20*%20%7Bwd%3A" +\
-	image_id + "%20wdt%3AP18%20%3Fimage.%7D%0A"
-	#"Q122677095" +\
-	
-	if httpr.request_completed.is_connected(_building_request_completed):
-		httpr.request_completed.disconnect(_building_request_completed)
-	httpr.request_completed.connect(_image_request_completed)
+	##if httpr.request_completed.is_connected(_building_request_completed):
+		##httpr.request_completed.disconnect(_building_request_completed)
+	#httpr.request_completed.disconnect
+	set_httpsignal(_image_id_request_completed)
 	
 	httpr.request(image_query, ["Accept: application/sparql-results+json"], HTTPClient.METHOD_GET)
+
+func _image_id_request_completed(a, b, c, result_body : PackedByteArray):
+	
+	var json = JSON.new()
+	json.parse(result_body.get_string_from_utf8())
+	var response = json.get_data()
+	
+	var response_body = response["results"]["bindings"]
+	
+	for image in response_body:
+		if "image" in image:
+			#associated_pics.append(image["object"]["value"])
+			#pics_urls[image["object"]["value"]] = image["image"]["value"]
+			$ItemList.add_item(image["objectLabel"]["value"])
 
 func get_pics_depicting_building(building_id):
 	
